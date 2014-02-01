0d8f01090373d3b5e8ad752b1205f6dc95332cc0
odanielson <olov.danielson@gmail.com>
2014-02-01 14:36:10 +0100

    Rename greenlets to greenlet_map.

--- geark/greenletmanager.py
+++ geark/greenletmanager.py
@@ -11,16 +11,16 @@ class GreenletAlreadyExistsError(Exception):
 class GreenletManager(object):
 
     def __init__(self):
-        self.greenlets = {}
+        self.greenlet_map = {}
 
     def start_greenlet(self, key, parent, auto_restart, function,
                        *args, **kwargs):
 
-        if key in self.greenlets:
+        if key in self.greenlet_map:
             raise GrennletAlreadyExistsError(
                 "Greenlet with key %s already exists." % (key))
 
-        self.greenlets[key] = {
+        self.greenlet_map[key] = {
             "greenlet": gevent.spawn(function, *args, **kwargs),
             "children": [],
             "auto_restart": auto_restart,
@@ -30,14 +30,14 @@ class GreenletManager(object):
 
     def list_greenlets(self):
         """Returns a list with greenlet keys."""
-        return self.greenlets.keys()
+        return self.greenlet_map.keys()
 
     def stop_greenlet(self, key):
 
-        greenlet = self.greenlets.get(key)
+        greenlet = self.greenlet_map.get(key)
         if greenlet:
             gevent.kill(greenlet["greenlet"])
-            del self.greenlets[key]
+            del self.greenlet_map[key]
             log.info("Stopped greenlet %s" % key)
 
 
