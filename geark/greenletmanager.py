3cba00876cc687f8aedecbbd5d74408d3d28e598
odanielson <olov.danielson@gmail.com>
2014-02-01 15:00:06 +0100

    Add test cases and functionality for restarting greenlets.

--- geark/greenletmanager.py
+++ geark/greenletmanager.py
@@ -15,19 +15,35 @@ class GreenletManager(object):
     def __init__(self):
         self.greenlet_map = {}
 
+    def _run_greenlet(self, key, auto_restart, function, *args, **kwargs):
+
+        while True:
+            try:
+                function(*args, **kwargs)
+                log.warning("greenlet \"%s\" stopped gracefully." % key)
+            except Exception as e:
+                log.warning("greenlet \"%s\" raised: %s" % (key, e))
+                # TODO: dump a stacktrace here
+
+            if not auto_restart:
+                break
+
+            log.info("restarting greenlet \"%s\"" % key)
+
     def start_greenlet(self, key, parent, auto_restart, function,
                        *args, **kwargs):
 
         if key in self.greenlet_map:
             raise GreenletAlreadyExistsError(
-                "Greenlet with key %s already exists." % (key))
+                "Greenlet \"%s\" already exists." % (key))
 
         self.greenlet_map[key] = {
-            "greenlet": gevent.spawn(function, *args, **kwargs),
+            "greenlet": gevent.spawn(self._run_greenlet, key, auto_restart,
+                                     function, *args, **kwargs),
             "children": [],
             "auto_restart": auto_restart,
             "restart_count": 0}
-        log.info("Started greenlet %s" % key)
+        log.info("Started greenlet \"%s\"" % key)
 
     def list_greenlets(self):
         """Returns a list with greenlet keys."""
@@ -39,7 +55,9 @@ class GreenletManager(object):
         if greenlet:
             gevent.kill(greenlet["greenlet"])
             del self.greenlet_map[key]
-            log.info("Stopped greenlet %s" % key)
+            log.info("Stopped greenlet \"%s\"" % key)
+
+
 
 
 instance = GreenletManager()
--- geark/test/test_greenletmanager.py
+++ geark/test/test_greenletmanager.py
@@ -11,30 +11,28 @@ from geark import greenletmanager
 class GreenletManagerHappyPathTestCase(unittest.TestCase):
 
     def setUp(self):
-        logging.basicConfig(level=logging.DEBUG)
+#        logging.basicConfig(level=logging.DEBUG)
+        self.ping = Queue()
+        self.pong = Queue()
 
     def _release_context(self, dt=0):
         gevent.sleep(dt)
 
     def test_starting_greenlet(self):
         """Verify that a greenlet can be started."""
-        queue = Queue()
 
         def loop(queue):
             queue.put("hello")
 
         greenletmanager.instance.start_greenlet(
-            "testloop", None, False, loop, queue)
+            "testloop", None, False, loop, self.ping)
         self._release_context()
 
-        msg = queue.get()
+        msg = self.ping.get()
         self.assertEquals(msg, "hello")
 
     def test_stopping_greenlet(self):
         """Verify that a greenlet can be stopped."""
-
-        ping = Queue()
-        pong = Queue()
 
         def ping_pong(ping, pong):
             while True:
@@ -42,13 +40,56 @@ class GreenletManagerHappyPathTestCase(unittest.TestCase):
                 pong.put(msg)
 
         greenletmanager.instance.start_greenlet(
-            "pingpongloop", None, False, ping_pong, ping, pong)
+            "pingpongloop", None, False, ping_pong, self.ping, self.pong)
         self._release_context()
 
-        ping.put("hello")
-        self.assertEquals("hello", pong.get())
+        self.ping.put("hello")
+        self.assertEquals("hello", self.pong.get())
 
         greenletmanager.instance.stop_greenlet("pingpongloop")
 
-        ping.put("hello again")
-        self.assertRaises(Empty, pong.get, timeout=0.01)
+        self.ping.put("hello again")
+        self.assertRaises(Empty, self.pong.get, timeout=0.01)
+
+    def test_graceful_stopping_greenlet_restarts(self):
+        """Verify that a greenlet that stops gracefully is restarted."""
+
+        def stopping_ping_pong(ping, pong):
+            msg = ping.get()
+            pong.put(msg)
+
+        greenletmanager.instance.start_greenlet(
+            "stoppingpingpongloop", None, True, stopping_ping_pong, self.ping,
+            self.pong)
+        self._release_context()
+
+        self.ping.put("hello")
+        self.assertEquals("hello", self.pong.get())
+
+        # The first ping-pong stopped the greenlet,
+        # so let's make sure it's up again!
+
+        self.ping.put("hello")
+        self.assertEquals("hello", self.pong.get())
+
+    def test_crashing_greenlet_restarts(self):
+        """Verify that a greenlet that crashes is restarted."""
+
+        def crashing_ping_pong(ping, pong):
+            msg = ping.get()
+            pong.put(msg)
+            raise Exception("Crash bing bong!")
+
+        greenletmanager.instance.start_greenlet(
+            "crashingpingpongloop", None, True, crashing_ping_pong, self.ping,
+            self.pong)
+        self._release_context()
+
+        self.ping.put("hello")
+        self.assertEquals("hello", self.pong.get())
+
+        # The first ping-pong stopped the greenlet,
+        # so let's make sure it's up again!
+
+        self.ping.put("hello")
+        self.assertEquals("hello", self.pong.get())
